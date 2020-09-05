b8508aeb00f64e5977253ea83672f292f065f3f7
Glepa <pgl_vs@mail.ru>
2020-09-05 15:49:37 +0500

    05.09.2020

--- CassWork_Online/05.09.2020.h
+++ CassWork_Online/05.09.2020.h
@@ -1,1 +1,72 @@
 #pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include<iterator>
+using namespace std;
+ 
+class SearchWord;
+class Dictonary {
+	vector <string> word;
+public:
+	Dictonary(const string str) {
+		bool n = true;
+		for (size_t i = 0; i < str.size(); i++)
+		{
+			if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) || str[i] == 33 ||
+				str[i] == 34 || str[i] == 45 || str[i] == 58 || str[i] == 59 || str[i] == 63)
+				continue;
+			else n = false;
+		}
+		if (n) word.push_back(str);
+		else cout << "\nErr!Is not word!";
+	}
+	 void addWord(const string str) {
+		 bool n = true;
+		 for (size_t i = 0; i < str.size(); i++)
+		 {
+			 if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) || str[i] == 33 ||
+				 str[i] == 34 || str[i] == 45 || str[i] == 58 || str[i] == 59 || str[i] == 63)
+				 continue;
+			 else n = false;
+		 }
+		 if(n) word.push_back(str);
+		 else cout << "\nErr!Is not word!";
+	}
+	 void delWord(const string str) {
+		 vector<string>::iterator iter = word.begin();
+		 for (size_t i = 0; i < word.size(); i++)
+		 {
+			 if (word[i] == str) {
+				 word.erase((iter += i));
+				 iter = word.begin();
+			 }
+		 }
+	 }
+	 void delWord(int i) {
+		 vector<string>::iterator iter = word.begin();
+		 word.erase((iter += i));
+	 }
+	 void show() {
+		 for (size_t i = 0; i < word.size(); i++)
+		 {
+			 cout << word[i];
+		 }
+	 }
+	 int size() {
+		 return word.size();
+	 }
+	 friend SearchWord;
+};
+
+class SearchWord {
+public:
+	bool operator () (const Dictonary& dic, const string str) {
+		for (size_t i = 0; i < dic.word.size(); i++)
+		{
+			if (dic.word[i] == str) return true;
+		}
+		return false;
+	}
+};
+
--- CassWork_Online/CW_Online.cpp
+++ CassWork_Online/CW_Online.cpp
@@ -5,23 +5,32 @@
 //#include"04.07.2020.h"
 //#include"18.07.2020.h"
 //#include"25.07.2020.h"
-#include"29.08.20.h"
+//#include"29.08.20.h"
+#include"05.09.2020.h"
 #include<iostream>
 
 using namespace std;
 int main() {
-	int size = 25;
-	int* arr = new int[size];
-	for (size_t i = 0; i < size; i++)
-	{
-		arr[i] = i + 1;
-	}
-	Stack <int> st(arr, size);
-	try {
-		st.add(27);
-	}
-	catch (string str) {
-		cout << "\n Err! " << str;
-	}
+	//int size = 25;
+	//int* arr = new int[size];
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	arr[i] = i + 1;
+	//}
+	//Stack <int> st(arr, size);
+	//try {
+	//	st.add(27);
+	//}
+	//catch (string str) {
+	//	cout << "\n Err! " << str;
+	//}
+	Dictonary dic("Hello");
+	dic.addWord("World");
+	dic.addWord("p_pot");
+	SearchWord search; 
+	if (search(dic, "Hello")) cout << "\nOk!";
+	else cout << "\n No word!";
+	if (search(dic, "Word")) cout << "\nOk!";
+	else cout << "\n No word!";
 }
 
